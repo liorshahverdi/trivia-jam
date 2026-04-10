580eb349fc209d18ceed81e26bfc418f0b39c35f
Lior Shahverdi <liorshahverdi@gmail.com>
2026-04-10 00:20:31 -0400

    next fix

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/trivia_jam/',
+  base: '/trivia-jam/',
   plugins: [react()],
   server: {
     host: true,
